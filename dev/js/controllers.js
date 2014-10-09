6eb370c8e81546253a3184e0af3747e9e984ff95
hichigo <hichigo91@gmail.com>
2014-10-09 17:29:11 +0400

    добавил анимацию

--- dev/js/controllers.js
+++ dev/js/controllers.js
@@ -23,4 +23,31 @@ nexusCtrl.controller('applicationCtrl', ['$location', '$scope',
 nexusCtrl.controller('aboutCtrl', ['$location', '$scope',
 	function($location, $scope) {
 		$scope.labels = ['Ваше имя', 'Ваш E-mail', 'Ваш номер (необязательно)'];
+		$scope.social = [
+			{
+				id: 'twitter',
+				xlink: '#twitter',
+				url: '#',
+			},
+			{
+				id: 'google',
+				xlink: '#google',
+				url: 'https://plus.google.com/u/0/116298012878227766967/about',
+			},
+			{
+				id: 'facebook',
+				xlink: '#facebook',
+				url: 'https://www.facebook.com/nexus.studio.bel/timeline#',
+			},
+			{
+				id: 'vk',
+				xlink: '#vk',
+				url: 'http://vk.com/nexusstudioru',
+			},
+			{
+				id: 'youtube',
+				xlink: '#youtube',
+				url: 'http://www.youtube.com/channel/UCbWoURkJ5nXueJQjWpXEraA?guided_help_flow=3',
+			}
+		];
 }]);
