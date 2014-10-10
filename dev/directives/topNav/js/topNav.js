4fedfb2a28a8147d3a5f536aea4336b1d29f72d6
hichigo <hichigo91@gmail.com>
2014-10-10 18:09:01 +0400

    одно меню, фтшьфция в меню и полоска на главной

--- dev/directives/topNav/js/topNav.js
+++ dev/directives/topNav/js/topNav.js
@@ -5,6 +5,23 @@ nexusApp.directive('topNav', function() {
 		replace: true,
 		controller: function($scope) {
 			
+		},
+		link: function($scope, $el) {
+			$el.find('.menu').append('<div class="bar-nav"></div>');
+			var bar = $el.find('.menu > .bar-nav'),
+					link = $el.find('.menu > a');
+			
+			$el.find('.menu').on('click', 'a', function() {
+				link.removeClass('active-btn');
+				$(this).addClass('active-btn');
+				var w = $(this).innerWidth(),
+						left = $(this).position().left;
+				
+				bar.css({
+					'width': w+'px',
+					'left': left+'px'
+				});
+			});
 		}
   };
 });
