d117c13d89f5d395c1b93dacb2f6b3d6f7fac688
hichigo <hichigo91@gmail.com>
2014-10-11 12:45:50 +0400

    анимации на все страницы и css поправил

--- dev/directives/topNav/js/topNav.js
+++ dev/directives/topNav/js/topNav.js
@@ -1,4 +1,4 @@
-nexusApp.directive('topNav', function() {
+nexusApp.directive('topNav', ['$location', function($location) {
   return {
 		restrict: 'E',
     templateUrl: 'dev/directives/topNav/tpl/top-nav.html',
@@ -7,10 +7,46 @@ nexusApp.directive('topNav', function() {
 			
 		},
 		link: function($scope, $el) {
-			$el.find('.menu').append('<div class="bar-nav"></div>');
 			var bar = $el.find('.menu > .bar-nav'),
-					link = $el.find('.menu > a');
+					link = $el.find('.menu > a'),
+					url = $location.$$url.slice(1);
 			
+			setTimeout(function(){
+				switch(url) {
+					case 'home':
+						bar.css({
+							'width': link.eq(0).innerWidth()+'px',
+							'left': link.eq(0).position().left+'px'
+						});
+						break;
+					case 'site':
+						bar.css({
+							'width': link.eq(1).innerWidth()+'px',
+							'left': link.eq(1).position().left+'px'
+						});
+						break;
+					case '3dgraphic':
+						bar.css({
+							'width': link.eq(2).innerWidth()+'px',
+							'left': link.eq(2).position().left+'px'
+						});
+						break;
+					case 'application':
+						bar.css({
+							'width': link.eq(3).innerWidth()+'px',
+							'left': link.eq(3).position().left+'px'
+						});
+						break;
+					case 'about':
+						bar.css({
+							'width': link.eq(4).innerWidth()+'px',
+							'left': link.eq(4).position().left+'px'
+						});
+						break;
+				}
+			},1000);
+			
+			console.log(url);
 			$el.find('.menu').on('click', 'a', function() {
 				link.removeClass('active-btn');
 				$(this).addClass('active-btn');
@@ -24,4 +60,4 @@ nexusApp.directive('topNav', function() {
 			});
 		}
   };
-});
+}]);
